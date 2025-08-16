6ea98da8e7cc24754f05801c4c2b73e52dbd9f66
Peter <peter.dereu@telenet.be>
2025-08-16 17:37:23 +0200

    Update SE_vocabulary_online.py

--- SE_vocabulary_online.py
+++ SE_vocabulary_online.py
@@ -12,7 +12,7 @@ st.set_page_config(
 # 🔹 Extra HTML voor iOS snelkoppeling icoon
 st.markdown(
     """
-    <link rel="apple-touch-icon" sizes="180x180" href="icoon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://github.com/PythonPeters/Zweeds_woordenschat/blob/main/icoon.png">
     """,
     unsafe_allow_html=True
     )
@@ -189,5 +189,6 @@ if uploaded_file:
 
 else:
     st.info("⬆️ Upload een Excelbestand om te beginnen.")
+
 
 
